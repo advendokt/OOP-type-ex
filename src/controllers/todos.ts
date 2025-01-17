dcf6fac27bd6fd75c0eaa3acaba4e9d04ed2e4bd
advendokt <a2vend0kt@gmail.com>
2025-01-17 13:45:31 +0300

    Update the body by id

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { Todo } from '../models/todo'
+import { hasUncaughtExceptionCaptureCallback } from "process";
 
 const todos: Todo[] = []
 
@@ -23,6 +24,27 @@ export const getTodos = (req: Request, res:Response, next: NextFunction) => {
             task: todos
         })
     }   catch(error) {
+        console.log(error)
+    }
+}
+
+export const updateTodo = (req: Request, res:Response, next: NextFunction) => {
+    try{
+        const todoId = req.params.id
+        const updateTask =(req.body as {task: string}).task
+        const todoIndex = todos.findIndex(todo => todo.id == todoId)
+
+        if(todoIndex < 0) {
+            throw new Error('Cloud not find todo with such id')
+        }
+
+        todos[todoIndex] = new Todo(todos[todoIndex].id, updateTask)
+
+        res.status(201).json({
+            message: 'Todo is update!',
+            updatedTask: todos[todoIndex]
+        })
+    }   catch(error){
         console.log(error)
     }
 }
