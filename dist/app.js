2b9b15788f72b1514f0e232e6a232090845d0b45
advendokt <a2vend0kt@gmail.com>
2025-01-17 12:25:37 +0300

    Created new Todo from user data

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,17 @@
-import express from 'express'
-
-const app = express()
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const todos_1 = __importDefault(require("./routes/todos"));
+const body_parser_1 = require("body-parser");
+const app = (0, express_1.default)();
+app.use('/todos', todos_1.default);
+app.use((0, body_parser_1.json)());
+app.use((error, req, res, next) => {
+    res.json({ messasge: error.message });
+});
 app.listen(3011, () => {
-    console.log('Server is running on http://localhost:3011')
-})
+    console.log('Server is running on http://localhost:3011');
+});
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response, NextFunction }from 'express'
 import todoRoutes from './routes/todos'
 import exp from 'constants'
+import { json } from 'body-parser'
 
 const app = express()
 
 app.use('/todos', todoRoutes)
+
+app.use(json())
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.json({messasge: error.message})
